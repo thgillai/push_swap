ddaaadab1915a37ba0b26dcaf5a107b91fece1e5
Thomas Gillain <tomgillain@gmail.com>
2021-05-28 14:35:29 +0200

    add is_sort function

--- main/main2.c
+++ main/main2.c
@@ -3,14 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   main2.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shenquin <shenquin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: thgillai <thgillai@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/27 16:38:58 by shenquin          #+#    #+#             */
-/*   Updated: 2021/05/27 16:43:37 by shenquin         ###   ########.fr       */
+/*   Updated: 2021/05/28 12:25:17 by thgillai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
+
+int	is_sort(int *pile)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (pile[i])
+	{
+		j = i;
+		while (pile[j])
+		{
+			if (pile[i] > pile[j])
+				return (-1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
 
 void	pile_doublons(int arg, t_pile *pile)
 {
@@ -48,13 +69,13 @@ void	pile_arg(char *arg, t_pile *pile)
 
 int	algo_main(t_pile *pile)
 {
+	if (!is_sort(pile->a))
+		return (0);
 	if (pile->arg_nb_a <= 3)
 		algo_3nba(pile->a, pile->arg_nb_a);
-	if (pile->arg_nb_a <= 5 && pile->arg_nb_a >= 4)
+	else if (pile->arg_nb_a <= 5 && pile->arg_nb_a >= 4)
 		algo_5nb(pile);
 	else
-	{
 		algo(pile);
-	}
 	return (0);
 }
