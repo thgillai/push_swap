984404e898d6b5cc51c195e06a97903de9a3932e
Alessio Glorioso <aglorios@MBP-de-Alessio.lan>
2021-06-05 00:15:57 +0200

    recherche d'opti

--- src/algo.c
+++ src/algo.c
@@ -55,12 +55,17 @@ int	push_to_b(t_pile *pile, t_data *data, int i, int j)
 	end = ((pile->arg_nb_a / data->chunk_nb) * (j + 1));
 	data->chunk_size = end - start;
 	data->refchunk = tabncpy(data->ref, start, end, pile->arg_nb_a);
-	while (i--)
+	while (i--) ////////////////////// trouver la position de l occurence la plus proche et agir en fonction au lieu de tourner en rond ??
 	{
 		if (occurence(data->refchunk, pile->a[0], data->chunk_size) == 1 )
 		{
 			push_b(pile);
 			ft_putstr_fd("pb\n", 1);
+	//		if (pile->b[0] < ft_max(pile->b)) // Pas dingue (rien avoir avec le com au dessus) 
+	//		{
+	//			rotate(pile->b, pile->arg_nb_b);
+	//			ft_putstr_fd("rb\n", 1);
+	//		}
 		}
 		else if (i)
 		{
@@ -73,7 +78,7 @@ int	push_to_b(t_pile *pile, t_data *data, int i, int j)
 
 int	push_to_a(t_pile *pile, t_data *data, int i)
 {	
-	while (pile->arg_nb_b)
+	while (pile->arg_nb_b) ////// je sais pas trop si on peut opti ici, plutot essayer d'avoir b un peu plus trier avant ??
 	{
 		data->max = ft_max(pile->b);
 		i = ft_pos(pile->b, data->max, pile->arg_nb_b);
@@ -149,9 +154,21 @@ int	last_chunk(t_pile *pile, t_data *data, int i)
 		ft_putstr_fd("pb\n", 1);
 	}
 	push_to_a_last_chunk(pile, data, i);
+//	data->min = ft_min(pile->a);
+//	i = ft_pos(pile->a, data->min, pile->arg_nb_a);
 	while (pile->a[0] != ft_min(pile->a))
 	{
-		rotate(pile->a, pile->arg_nb_a);
+/*		if (i > pile->arg_nb_a / 2) // trop lent avec 500 mais pas mal avec 100 (opti le 500 avec 5x100 ?)
+		{
+			rot_rot(pile->a, pile->arg_nb_a);
+			ft_putstr_fd("rra\n", 1);
+		}
+		else
+		{
+			rotate(pile->a, pile->arg_nb_a);
+			ft_putstr_fd("ra\n", 1);
+		}
+*/		rotate(pile->a, pile->arg_nb_a);
 		ft_putstr_fd("ra\n", 1);
 	}
 	return (0);
